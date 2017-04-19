08d562497626231c6f44ea197e7f4175b32443b3
Amanuel Zewdu <azewdu87@gmail.com>
2017-04-18 17:13:42 -0800

    the code that changes pdf  to text

---
+++ Amanuel/PDF to HTML.py
@@ -0,0 +1,30 @@
+#pdf2txt.py
+import sys
+import pyPdf
+import os
+
+def getPDFContent(path):
+    content = ""
+    path = 'C:/Users/Amanuel/Desktop/new/Doc.pdf'
+# Load PDF into pyPDF
+    pdf = pyPdf.PdfFileReader(file(path, 'rb'))
+# Iterate pages
+    for i in range(0, pdf.getNumPages()):
+# Extract text from page and add to content
+        content += pdf.getPage(i).extractText() + " \n"
+# Collapse whitespace
+# content = u" ".join(content.replace(u"\xa0", u" ").strip().split())
+    return content
+
+
+pdf = sys.argv[0]
+filedir,filename = os.path.split(pdf)
+nameonly = os.path.splitext(filename)
+newname = nameonly[0] + ".txt"
+outtxt = os.path.join(filedir,newname)
+f = open(outtxt,'w')
+f.write(getPDFContent(pdf))
+f.close()
+print f
+
+#exit()
